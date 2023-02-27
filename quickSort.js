ce5f14b9a819b31fd2338b7d63cb289e01ef6e53
adnane abid <adnaneabid@adnanes-MacBook-Pro.local>
2023-02-27 16:07:57 -0500

    Adding Snippets

--- quickSort.js
+++ quickSort.js
@@ -77,3 +77,33 @@ function quickSort(array) {
 
 // Worst case will be Big-O = O(n^2)
 // Avg case is O(nlogn) because we got 2 arrays = O(logn) plus a loop.
+
+function quickSort(arr, s, e) {
+  if (e - s + 1 <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[e];
+  let left = s; // pointer for left side
+
+  // Partition: elements smaller than pivot on left side
+  for (let i = s; i < e; i++) {
+    if (arr[i] < pivot) {
+      let temp = arr[left];
+      arr[left] = arr[i];
+      arr[i] = temp;
+      left++;
+    }
+  }
+  // Move pivot in-between left & right sides
+  arr[e] = arr[left];
+  arr[left] = pivot;
+
+  //Quick sort left side
+  quickSort(arr, s, left - 1);
+
+  //Quick sort right side
+  quickSort(arr, left + 1, e);
+
+  return arr;
+}
